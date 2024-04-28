e75f26ad76181514780e4b948ed8221f214fcee5
AndresCS20 <andrsc8@gmail.com>
2024-04-28 19:30:31 +0200

    Arreglado fondo web

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,12 +14,12 @@ export default {
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themeRoot: "html", // The element that receives theme color CSS variables
     themes: [
       {
         light: {
           ...require("daisyui/src/theming/themes")["light"],
-          "background": "#0071eb !important",
+          "--bg-base-bg": "#0071eb",
           ".color-logo": {
             "color": "#fff",
           },
@@ -29,133 +29,133 @@ export default {
         },
         dark:{
           ...require("daisyui/src/theming/themes")["dark"],
-          "background": "#0071eb",
+          "--bg-base-bg": "#0071eb",
         },
         cupcake:{
           ...require("daisyui/src/theming/themes")["cupcake"],
-          "background": "#c67299",
+          "--bg-base-bg": "#c67299",
         },
         bumblebee:{
           ...require("daisyui/src/theming/themes")["bumblebee"],
-          "background": "#d6782c",
+          "--bg-base-bg": "#d6782c",
         },
         emerald:{
           ...require("daisyui/src/theming/themes")["emerald"],
-          "background": "#6d7fd2",
+          "--bg-base-bg": "#6d7fd2",
         },
         corporate:{
           ...require("daisyui/src/theming/themes")["corporate"],
-          "background": "#267ab6",
+          "--bg-base-bg": "#267ab6",
         },
         synthwave:{
           ...require("daisyui/src/theming/themes")["synthwave"],
-          "background": "#ffa5bf",
+          "--bg-base-bg": "#ffa5bf",
         },
         retro:{
           ...require("daisyui/src/theming/themes")["retro"],
-          "background": "#b57e6a",
+          "--bg-base-bg": "#b57e6a",
         },
         cyberpunk:{
           ...require("daisyui/src/theming/themes")["cyberpunk"],
-          "background": "#8779d5",
+          "--bg-base-bg": "#8779d5",
         },
         valentine:{
           ...require("daisyui/src/theming/themes")["valentine"],
-          "background": "#c178ba",
+          "--bg-base-bg": "#c178ba",
         },
         halloween:{
           ...require("daisyui/src/theming/themes")["halloween"],
-          "background": "#a75200",
+          "--bg-base-bg": "#a75200",
         },
         garden:{
           ...require("daisyui/src/theming/themes")["garden"],
-          "background": "#8b4a2a",
+          "--bg-base-bg": "#8b4a2a",
         },
         forest:{
           ...require("daisyui/src/theming/themes")["forest"],
-          "background": "#029672",
+          "--bg-base-bg": "#029672",
         },
         aqua:{
           ...require("daisyui/src/theming/themes")["aqua"],
-          "background": "#36bbf4",
+          "--bg-base-bg": "#36bbf4",
         },
         lofi:{
           ...require("daisyui/src/theming/themes")["lofi"],
-          "background": "#161515",
+          "--bg-base-bg": "#161515",
         },
         pastel:{
           ...require("daisyui/src/theming/themes")["pastel"],
-          "background": "#bbc7e9",
+          "--bg-base-bg": "#bbc7e9",
           ".color-logo": {
             "color": "#000",
           }
         },
         fantasy:{
           ...require("daisyui/src/theming/themes")["fantasy"],
-          "background": "#6d4ca0",
+          "--bg-base-bg": "#6d4ca0",
         },
         wireframe:{
           ...require("daisyui/src/theming/themes")["wireframe"],
-          "background": "#c5c5c5",
+          "--bg-base-bg": "#c5c5c5",
           ".color-logo": {
             "color": "#000",
           }
         },
         black:{
           ...require("daisyui/src/theming/themes")["black"],
-          "background": "#373737",
+          "--bg-base-bg": "#373737",
         },
         luxury:{
           ...require("daisyui/src/theming/themes")["luxury"],
-          "background": "#70525f",
+          "--bg-base-bg": "#70525f",
         },
         dracula:{
           ...require("daisyui/src/theming/themes")["dracula"],
-          "background": "#c272b0",
+          "--bg-base-bg": "#c272b0",
         },
         cmyk:{
           ...require("daisyui/src/theming/themes")["cmyk"],
-          "background": "#d3638a",
+          "--bg-base-bg": "#d3638a",
         },
         autumn:{
           ...require("daisyui/src/theming/themes")["autumn"],
-          "background": "#b05f46",
+          "--bg-base-bg": "#b05f46",
         },
         business:{
           ...require("daisyui/src/theming/themes")["business"],
-          "background": "#456490",
+          "--bg-base-bg": "#456490",
         },
         acid:{
           ...require("daisyui/src/theming/themes")["acid"],
-          "background": "#ff0066",
+          "--bg-base-bg": "#ff0066",
         },
         lemonade:{
           ...require("daisyui/src/theming/themes")["lemonade"],
-          "background": "#889400",
+          "--bg-base-bg": "#889400",
         },
         night:{
           ...require("daisyui/src/theming/themes")["night"],
-          "background": "#6e7cc8",
+          "--bg-base-bg": "#6e7cc8",
         },
         coffee:{
           ...require("daisyui/src/theming/themes")["coffee"],
-          "background": "#1f506d",
+          "--bg-base-bg": "#1f506d",
         },
         winter:{
           ...require("daisyui/src/theming/themes")["winter"],
-          "background": "#5141a3",
+          "--bg-base-bg": "#5141a3",
         },
         dim:{
           ...require("daisyui/src/theming/themes")["dim"],
-          "background": "#c272ac",
+          "--bg-base-bg": "#c272ac",
         },
         nord:{
           ...require("daisyui/src/theming/themes")["nord"],
-          "background": "#6b8dab",
+          "--bg-base-bg": "#6b8dab",
         },
         sunset:{
           ...require("daisyui/src/theming/themes")["sunset"],
-          "background": "#b562a5",
+          "--bg-base-bg": "#b562a5",
         }
       }
     ]
