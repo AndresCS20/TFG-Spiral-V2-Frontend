9dfb3dc5a8d80895582d4f9c7acde2e0ada37f6c
AndresCS20 <andrsc8@gmail.com>
2024-06-01 00:10:03 +0200

    Añadida parcialmente la funcionalidad de reacciones a las publicaciones (se puede ver los tres tipos de reacciones que mas han reaccionado) y la reaccion tuya activa

--- src/app/components/shared/elements/publication/publication.component.ts
+++ src/app/components/shared/elements/publication/publication.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, signal } from '@angular/core';
 import { AvatarFrameComponent } from '../avatar-frame/avatar-frame.component';
-import { BodyReaction, Publication } from '@interfaces/publications.interface';
+import { BodyReaction, Publication, ReactionReaction } from '@interfaces/publications.interface';
 import { StorageService } from '@services/storage.service';
 import { RouterLink } from '@angular/router';
 import moment from 'moment';
@@ -18,7 +18,10 @@ export class PublicationComponent implements OnInit{
   constructor(private storageServie: StorageService) {}
   @Input() publication!: Publication
   user : any
-  // reactions = signal<BodyReaction>([])
+  reactions = signal<BodyReaction[]>([])
+  activeReaction = signal<BodyReaction | null>(null) // Se guarda el type de reacción activa
+  reactionCount = signal(0)
+  mostReacted = signal<BodyReaction[]>([])
 
   customOptions: OwlOptions = {
     loop: true,
@@ -35,7 +38,42 @@ export class PublicationComponent implements OnInit{
 
   ngOnInit(): void {
     this.user = this.storageServie.getUser()
+    if(this.publication){
+      let reactCount=0
+      this.reactions.set(this.publication.reactions)
+ 
+      this.publication.reactions.sort((a, b) => b.reactions.length - a.reactions.length);
+      this.mostReacted.set(this.publication.reactions.slice(0, 3))
+      
+      for(let i = 0; i < this.reactions().length; i++){
+        reactCount+=this.reactions()[i].reactions.length
+      }
+      this.reactionCount.set(reactCount)
+      this.activeReaction.set(this.hasUserReacted())
+     if(this.activeReaction()){
+      console.log("this.activeReaction()", this.activeReaction())
+     }
+    }
   }
+  hasUserReacted(): BodyReaction | null {
+    console.log("ID USU:", this.user._id)
+    for (let reaction of this.publication.reactions) {
+      reaction.reactions.some(userReaction => {
+        console.log(userReaction.user._id, this.user._id)
+      })
+      if (reaction.reactions.some(userReaction => userReaction.user._id === this.user._id)) {
+        console.log("REAZION", reaction)
+        return reaction;
+      }
+    }
+    return null;
+  }
+  
+  // hasUserReacted(type: string): boolean {
+  //   return this.publication.reactions.some(reaction => 
+  //     reaction.reactions.some(userReaction => userReaction.user === this.user._id)
+  //   );
+  // }
 
   dateFormatted(): string {
     return moment(this.publication.createdAt).locale('es').fromNow();
